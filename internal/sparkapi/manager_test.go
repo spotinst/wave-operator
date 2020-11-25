2568603b6c0ab0f9f37b8fd4bd42576b2deb4166
thorsteinnth <thorsteinnth@gmail.com>
2020-11-25 17:12:38 +0100

    manager tests

---
+++ internal/sparkapi/manager_test.go
@@ -0,0 +1,136 @@
+package sparkapi
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/go-logr/logr"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	sparkapiclient "github.com/spotinst/wave-operator/internal/sparkapi/client"
+	"github.com/spotinst/wave-operator/internal/sparkapi/client/mock_client"
+)
+
+func TestGetApplicationInfo(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	applicationId := "spark-123"
+
+	t.Run("whenError", func(tt *testing.T) {
+
+		m := mock_client.NewMockClient(ctrl)
+		m.EXPECT().GetApplication(applicationId).Return(getApplicationResponse(), nil).Times(1)
+		m.EXPECT().GetEnvironment(applicationId).Return(nil, fmt.Errorf("test error")).Times(1)
+		m.EXPECT().GetStages(applicationId).Return(getStagesResponse(), nil).Times(0)
+
+		manager := &manager{
+			client: m,
+			logger: getTestLogger(),
+		}
+
+		_, err := manager.GetApplicationInfo(applicationId)
+		assert.Error(tt, err)
+		assert.Contains(tt, err.Error(), "test error")
+
+	})
+
+	t.Run("whenSuccessful", func(tt *testing.T) {
+
+		m := mock_client.NewMockClient(ctrl)
+		m.EXPECT().GetApplication(applicationId).Return(getApplicationResponse(), nil).Times(1)
+		m.EXPECT().GetEnvironment(applicationId).Return(getEnvironmentResponse(), nil).Times(1)
+		m.EXPECT().GetStages(applicationId).Return(getStagesResponse(), nil).Times(1)
+
+		manager := &manager{
+			client: m,
+			logger: getTestLogger(),
+		}
+
+		res, err := manager.GetApplicationInfo(applicationId)
+		assert.NoError(tt, err)
+
+		assert.Equal(tt, "my-test-application", res.ApplicationName)
+
+		assert.Equal(tt, int64(900), res.TotalExecutorCpuTime)
+		assert.Equal(tt, int64(500), res.TotalInputBytes)
+		assert.Equal(tt, int64(700), res.TotalOutputBytes)
+
+		assert.Equal(tt, 2, len(res.SparkProperties))
+		assert.Equal(tt, "val1", res.SparkProperties["prop1"])
+		assert.Equal(tt, "val2", res.SparkProperties["prop2"])
+
+		assert.Equal(tt, 2, len(res.Attempts))
+		assert.Equal(tt, getApplicationResponse().Attempts[0], res.Attempts[0])
+		assert.Equal(tt, getApplicationResponse().Attempts[1], res.Attempts[1])
+	})
+}
+
+func getTestLogger() logr.Logger {
+	return zap.New(zap.UseDevMode(true))
+}
+
+func getApplicationResponse() *sparkapiclient.Application {
+	return &sparkapiclient.Application{
+		Id:   "spark-123",
+		Name: "my-test-application",
+		Attempts: []sparkapiclient.Attempt{
+			{
+				StartTimeEpoch:   100,
+				EndTimeEpoch:     200,
+				LastUpdatedEpoch: 300,
+				Duration:         400,
+				SparkUser:        "spark-user",
+				Completed:        false,
+				AppSparkVersion:  "3.0.0",
+			},
+			{
+				StartTimeEpoch:   500,
+				EndTimeEpoch:     600,
+				LastUpdatedEpoch: 700,
+				Duration:         800,
+				SparkUser:        "spark-user-2",
+				Completed:        true,
+				AppSparkVersion:  "9.0.0",
+			},
+		},
+	}
+}
+
+func getStagesResponse() []sparkapiclient.Stage {
+	return []sparkapiclient.Stage{
+		{
+			StageId:         1,
+			AttemptId:       2,
+			InputBytes:      100,
+			OutputBytes:     200,
+			ExecutorCpuTime: 300,
+		},
+		{
+			StageId:         2,
+			AttemptId:       3,
+			InputBytes:      400,
+			OutputBytes:     500,
+			ExecutorCpuTime: 600,
+		},
+	}
+}
+
+func getEnvironmentResponse() *sparkapiclient.Environment {
+	return &sparkapiclient.Environment{
+		SparkProperties: [][]string{
+			{
+				"prop1", "val1",
+			},
+			{
+				"prop2", "val2",
+			},
+			{
+				"prop3", "val3", "thisShouldBeIgnored",
+			},
+		},
+	}
+}
