722a009995dd1546da5be81e3abd79cd78bb2689
Nate Franzen <natef@netapp.com>
2021-02-25 10:39:36 -0800

    Tide chart input (#29)
    adds a serializable helm chart InstallSpec in tide, prefer "latest" chart version

--- install/installer_test.go
+++ install/installer_test.go
@@ -1,6 +1,9 @@
 package install
 
 import (
+	"encoding/json"
+	"reflect"
+	"strconv"
 	"testing"
 
 	"github.com/spotinst/wave-operator/api/v1alpha1"
@@ -94,5 +97,69 @@ serviceAccount:
 	}
 	u = i.IsUpgrade(getValuesObjects(v1, v2))
 	assert.False(t, u)
+
+}
+
+func TestMarshal(t *testing.T) {
+	is := InstallSpec{
+		Name:       "wave-operator",
+		Repository: "https://charts.spot.io",
+		Version:    "0.2.1",
+		Values: `
+image:
+  repository: public.ecr.aws/l8m2k1n1/netapp/wave-operator
+  tag: "0.2.1-1d11e752"
+`,
+	}
+
+	spec, err := json.Marshal(is)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, spec)
+}
+
+func TestUnmarshal(t *testing.T) {
+
+	text := "{\"name\":\"wave-operator\",\"repository\":\"https://charts.spot.io\",\"version\":\"0.2.0\"}"
+	is := &InstallSpec{}
+
+	err := json.Unmarshal([]byte(text), is)
+	assert.NoError(t, err)
+	assert.NotNil(t, is)
+	assert.Equal(t, "wave-operator", is.Name)
+	assert.Equal(t, "https://charts.spot.io", is.Repository)
+	assert.Equal(t, "0.2.0", is.Version)
+	assert.Empty(t, is.Values)
+}
+
+func TestUnmarshalJsonValues(t *testing.T) {
+
+	// "values" can be a json string & it will unmarshal as yaml, so long as its quoted properly
+	values := "{\"image\":{\"repository\": \"public.ecr.aws/l8m2k1n1/netapp/wave-operator\",\"tag\": \"0.2.1-1d11e752\"}}"
+	text := "{\"name\":\"wave-operator\",\"repository\":\"https://charts.spot.io\",\"version\":\"0.2.0\",\"values\": " + strconv.Quote(values) + "}"
+	is := &InstallSpec{}
+
+	err := json.Unmarshal([]byte(text), is)
+	assert.NoError(t, err)
+	assert.NotNil(t, is)
+	assert.Equal(t, "wave-operator", is.Name)
+	assert.Equal(t, "https://charts.spot.io", is.Repository)
+	assert.Equal(t, "0.2.0", is.Version)
+	assert.NotEmpty(t, is.Values)
+
+	var vals map[string]interface{}
+	err = yaml.Unmarshal([]byte(is.Values), &vals)
+	assert.NoError(t, err)
+
+	expected := map[string]string{
+		"repository": "public.ecr.aws/l8m2k1n1/netapp/wave-operator",
+		"tag":        "0.2.1-1d11e752",
+	}
+	// assert.True(t, reflect.DeepEqual(expected, vals["image"]),
+	// 	fmt.Sprintf("      got %v\n expected %v", vals["image"], expected))
+	vals2, ok := vals["image"].(map[string]interface{})
+	assert.True(t, ok)
+	assert.Equal(t, "", reflect.TypeOf(vals["image"]).Name())
+	assert.Equal(t, expected["repository"], vals2["repository"])
+	assert.Equal(t, expected["tag"], vals2["tag"])
 
 }
