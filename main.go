5e7364de17ebfc8ac37a27cdbe6eed5d966c655c
Arnar Páll <arnar@netapp.com>
2021-04-14 15:15:40 +0000

    Removed additional whitespace

--- main.go
+++ main.go
@@ -58,7 +58,6 @@ func main() {
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-
 	flag.Parse()
 
 	logger := zap.New(zap.UseDevMode(true))
