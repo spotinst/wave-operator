e2c4f0816b1ce0dd3c7ee3cd6dbfadb426479204
Thorsteinn Thorri Sigurdsson <thorsteinnth@gmail.com>
2021-05-27 10:40:15 +0200

    parse instance type annotation

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,16 @@
 package config
 
 import (
+	"fmt"
 	"strconv"
+	"strings"
+
+	"github.com/go-logr/logr"
 )
 
 const (
 	WaveConfigAnnotationSyncEventLogs = "wave.spot.io/synceventlogs"
+	WaveConfigAnnotationInstanceType  = "wave.spot.io/instancetype"
 )
 
 func IsEventLogSyncEnabled(annotations map[string]string) bool {
@@ -17,4 +22,41 @@ func IsEventLogSyncEnabled(annotations map[string]string) bool {
 		return false
 	}
 	return enabled
+}
+
+func GetConfiguredInstanceTypes(annotations map[string]string, log logr.Logger) []string {
+	instanceTypes := make([]string, 0)
+	if annotations == nil {
+		return instanceTypes
+	}
+	conf := annotations[WaveConfigAnnotationInstanceType]
+	if conf == "" {
+		return instanceTypes
+	}
+	split := strings.Split(conf, ",")
+	for _, s := range split {
+		trimmed := strings.TrimSpace(s)
+		if validateInstanceType(trimmed) {
+			instanceTypes = append(instanceTypes, trimmed)
+		} else {
+			log.Info(fmt.Sprintf("Got invalid instance type %q, ignoring", trimmed))
+		}
+	}
+	return instanceTypes
+}
+
+// TODO(thorsteinn) Make sure that the instance type is valid in the cluster region,
+// and allowed in the cluster configuration
+func validateInstanceType(instanceType string) bool {
+	// Instance types should be of the form family.type (e.g. m5.xlarge)
+	split := strings.Split(instanceType, ".")
+	if len(split) != 2 {
+		return false
+	}
+	for _, s := range split {
+		if len(s) == 0 {
+			return false
+		}
+	}
+	return true
 }
