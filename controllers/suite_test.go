98fc53dd34fcd579d8dad51a6823367d5c3a5ece
Arnar Páll <arnar@netapp.com>
2021-06-04 12:20:00 +0000

    Fixing compilation issue with test

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -119,9 +119,7 @@ var _ = BeforeSuite(func(done Done) {
 		clientSet,
 		sparkapi.GetManager,
 		ctrl.Log.WithName("controllers").WithName("SparkPod"),
-		k8sManager.GetScheme(),
-		nil,
-	)
+		k8sManager.GetScheme())
 	err = sparkPodController.SetupWithManager(k8sManager)
 	Expect(err).ToNot(HaveOccurred())
 
