fc98ad9b50034ef58a9b8876d84e677f6aeab623
Thorsteinn Thorri Sigurdsson <thorsteinnth@gmail.com>
2021-06-10 10:48:19 +0200

    tests

--- internal/spot/client/aws_test.go
+++ internal/spot/client/aws_test.go
@@ -1,21 +1,84 @@
 package client
 
 import (
-	"fmt"
-	"github.com/spotinst/wave-operator/internal/logger"
-	"github.com/stretchr/testify/require"
+	"bytes"
+	"io"
+	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func AwsTestManual(t *testing.T) {
+func TestGetAvailableInstanceTypesInRegion(t *testing.T) {
 
-	c, err := NewClient(logger.New())
-	require.NoError(t, err)
+	t.Run("whenSuccessful", func(tt *testing.T) {
 
-	t.Run("GetInstanceTypes", func(t *testing.T) {
-		res, err := c.GetAvailableInstanceTypesInRegion("us-west-2")
-		require.NoError(t, err)
-		fmt.Println(res)
+		region := "my-region"
+
+		testTransport := transportTestFunc(func(req *http.Request) (*http.Response, error) {
+			assert.Equal(tt, region, req.URL.Query().Get("region"))
+			return &http.Response{
+				StatusCode: 200,
+				Body:       io.NopCloser(bytes.NewBufferString(getInstanceTypesInRegionResponse)),
+			}, nil
+		})
+
+		c := clientWithTestTransport(testTransport)
+
+		res, err := c.GetAvailableInstanceTypesInRegion(region)
+		require.NoError(tt, err)
+
+		expected := []*InstanceType{
+			{
+				InstanceType: "hi1.4xlarge",
+			},
+			{
+				InstanceType: "r5d.4xlarge",
+			},
+			{
+				InstanceType: "r5ad.12xlarge",
+			},
+			{
+				InstanceType: "r4.4xlarge",
+			},
+		}
+
+		assert.Equal(tt, expected, res)
+
 	})
 
 }
+
+var getInstanceTypesInRegionResponse = `
+{
+  "request": {
+    "id": "cd645d74-8b38-4d37-85d7-78f78b75bcd5",
+    "url": "/aws/ec2/spotType?region=us-west-2",
+    "method": "GET",
+    "timestamp": "2021-06-10T08:33:59.441Z"
+  },
+  "response": {
+    "status": {
+      "code": 200,
+      "message": "OK"
+    },
+    "kind": "spotinst:aws:ec2:spot:type",
+    "items": [
+      {
+        "instanceType": "hi1.4xlarge"
+      },
+      {
+        "instanceType": "r5d.4xlarge"
+      },
+      {
+        "instanceType": "r5ad.12xlarge"
+      },
+      {
+        "instanceType": "r4.4xlarge"
+      }
+    ],
+    "count": 4
+  }
+}
+`
--- internal/spot/client/client_test.go
+++ internal/spot/client/client_test.go
@@ -1,8 +1,29 @@
-package client_test
+package client
 
 import (
+	"net/http"
 	"testing"
+
+	"github.com/spotinst/wave-operator/internal/logger"
 )
+
+type transportTestFunc func(req *http.Request) (*http.Response, error)
+
+func (f transportTestFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req)
+}
+
+var clientWithTestTransport = func(testTransport transportTestFunc) *Client {
+	return &Client{
+		logger:                  logger.New(),
+		clusterIdentifier:       "my-cluster",
+		clusterUniqueIdentifier: "my-uuid",
+		httpClient: &http.Client{
+			Timeout:   requestTimeout,
+			Transport: testTransport,
+		},
+	}
+}
 
 func TestNewClient(t *testing.T) {
 	// TODO
