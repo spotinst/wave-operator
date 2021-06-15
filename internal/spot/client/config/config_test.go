c1196f7bd75e93dac422cc72ae63c5c0210af942
Thorsteinn Thorri Sigurdsson <thorsteinnth@gmail.com>
2021-06-15 09:56:50 +0200

    tests

--- internal/spot/client/config/config_test.go
+++ internal/spot/client/config/config_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sfake "k8s.io/client-go/kubernetes/fake"
 
 	"github.com/spotinst/wave-operator/internal/logger"
 	"github.com/spotinst/wave-operator/internal/ocean"
@@ -14,12 +16,16 @@ import (
 
 func getTestCM(data map[string]string) *corev1.ConfigMap {
 	cm := &corev1.ConfigMap{}
+	cm.Name = ocean.SpotinstOceanConfigmap
+	cm.Namespace = v1.NamespaceSystem
 	cm.Data = data
 	return cm
 }
 
 func getTestSecret(data map[string]string) *corev1.Secret {
 	secret := &corev1.Secret{}
+	secret.Name = ocean.SpotinstOceanSecret
+	secret.Namespace = v1.NamespaceSystem
 	secret.Data = make(map[string][]byte)
 	for k, v := range data {
 		secret.Data[k] = []byte(v)
@@ -54,6 +60,56 @@ func envsEqual(env1 []string, env2 []string) bool {
 		}
 	}
 	return true
+}
+
+func TestGetConfig(t *testing.T) {
+
+	log := logger.New()
+
+	t.Run("whenError", func(tt *testing.T) {
+
+		clientSet := k8sfake.NewSimpleClientset()
+		_, err := GetConfig(clientSet, log)
+		require.Error(tt, err)
+
+		cm := getTestCM(nil)
+		secret := getTestSecret(nil)
+		clientSet = k8sfake.NewSimpleClientset(cm, secret)
+		_, err = GetConfig(clientSet, log)
+		require.Error(tt, err)
+
+	})
+
+	t.Run("whenSuccessful", func(tt *testing.T) {
+
+		ns := &corev1.Namespace{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "kube-system",
+				UID:  "1234-5678",
+			},
+		}
+		cm := getTestCM(map[string]string{
+			ocean.SpotinstBaseURL:           "https://my-url.io",
+			ocean.SpotinstProxyURL:          "https://my-proxy-url.io",
+			ocean.SpotinstClusterIdentifier: "my-cluster-identifier",
+		})
+		secret := getTestSecret(map[string]string{
+			ocean.SpotinstToken:   "my-token",
+			ocean.SpotinstAccount: "my-account",
+		})
+		clientSet := k8sfake.NewSimpleClientset(ns, cm, secret)
+
+		res, err := GetConfig(clientSet, log)
+		require.NoError(tt, err)
+		assert.Equal(tt, "my-token", res.Creds.Token)
+		assert.Equal(tt, "my-account", res.Creds.Account)
+		assert.Equal(tt, "my-url.io", res.BaseURL.Host)
+		assert.Equal(tt, "my-proxy-url.io", res.ProxyConfig.HTTPSProxy.Host)
+		assert.Equal(tt, "my-cluster-identifier", res.ClusterIdentifier)
+		assert.Equal(tt, "1234-5678", res.ClusterUniqueIdentifier)
+
+	})
+
 }
 
 func TestGetClusterIdentifier(t *testing.T) {
